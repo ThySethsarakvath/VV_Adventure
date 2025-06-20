dde2955867d3e395045003b6e915917a46f22424
DoDavin <dodavin96@gmail,com>
2025-06-20 14:34:27 +0700

    Added

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,7 +8,6 @@ public class Main {
 	//VV Adventure main program
     public static void main(String[] args) {
     	
-    	
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
