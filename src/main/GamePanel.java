9beae7018fcc777b41368519248fe98d0fe739c6
vath <bro.vath7547@gmail.com>
2025-07-06 19:39:07 +0700

    Full screen Implementation

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
@@ -12,155 +15,206 @@ import entity.Player;
 import object.SuperObject;
 import tile.TileManager;
 
-public class GamePanel extends JPanel implements Runnable{
+public class GamePanel extends JPanel implements Runnable {
 
-	//Screen Panel
-	final int originalTileSize = 16; //16x 16
+	// Screen Panel
+	final int originalTileSize = 16; // 16x 16
 	final int scale = 3;
-	
-	public final int tileSize = originalTileSize*scale; //make element 48x48
+
+	public final int tileSize = originalTileSize * scale; // make element 48x48
 	public final int maxScreenRow = 12;
-	public final int maxScreenCol = 14;
-	public final int screenWidth = tileSize * maxScreenCol; //768 px of screen
-	public final int screenHeight = tileSize * maxScreenRow; //576 px
-	
+	public final int maxScreenCol = 16;
+	public final int screenWidth = tileSize * maxScreenCol; // 768 px of screen
+	public final int screenHeight = tileSize * maxScreenRow; // 576 px
+
 	// WORLD SETTINGS
 	public final int maxWorldCol = 105; // Changeable depends on world01.txt
 	public final int maxWorldRow = 100;
 	public final int worldWidth = tileSize * maxWorldCol; // Can be deleted
 	public final int worldHeight = tileSize * maxWorldRow;// Can be deleted
-	
-	//FPS
+
+	// Full Screen
+	int screenWidth2 = screenWidth;
+	int screenHeight2 = screenHeight;
+	BufferedImage tempScreen;
+	Graphics2D g2;
+	// FPS
 	int FPS = 60;
-	
+
 	TileManager tileM = new TileManager(this);
 	KeyHandler keyH = new KeyHandler();
-	
+
 	Sound sound = new Sound();
 	public CollisionChecker cChecker = new CollisionChecker(this);
 	public AssetSetter aSetter = new AssetSetter(this);
-	Thread gameThread; //to start and to stop
+	Thread gameThread; // to start and to stop
+
+	// Entity and Object
 	public Player player = new Player(this, keyH);
 	public SuperObject obj[] = new SuperObject[10];// object array
 	public Entity npc[] = new Entity[10];
-	
-	
-	
+
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
-		
-		/* 
-		 DoubleBuffered is a technique that draws graphics to an off-screen image first, 
-		 then displays it all at once to prevent flickering and make rendering smoother.
+
+		/*
+		 * DoubleBuffered is a technique that draws graphics to an off-screen image
+		 * first, then displays it all at once to prevent flickering and make rendering
+		 * smoother.
 		 */
-		this.setDoubleBuffered(true); 
+		this.setDoubleBuffered(true);
 		this.addKeyListener(keyH);
 		this.setFocusable(true);
 	}
-	
+
 	public void setupGame() {
 		aSetter.setObject();
 		aSetter.setNpc();
 		playMusic(0);
+
+		tempScreen = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_ARGB);
+		g2 = (Graphics2D) tempScreen.getGraphics();
+		
+		setFullScreen(); // command this if want to turn off fullscreen mode
 	}
 
+	public void setFullScreen() {
+		// Get screen size from local device
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice gd = ge.getDefaultScreenDevice();
+		gd.setFullScreenWindow(Main.window);
+		
+		// width and height of full screen
+		screenWidth2 = Main.window.getWidth();
+		screenHeight2 = Main.window.getHeight();
+		
+		}
 	public void startGameThread() {
 		gameThread = new Thread(this);
 		gameThread.start();
 	}
-	
-	//delta method
+
+	// delta method
 	public void run() {
-		
+
 		double drawInterval = 1000000000 / FPS;
 		double delta = 0;
 		long lastTime = System.nanoTime();
 		long currentTime;
 		long timer = 0;
 		int drawCount = 0;
-		
-		while(gameThread != null) {
-			
+
+		while (gameThread != null) {
+
 			currentTime = System.nanoTime();
-			
+
 			delta += (currentTime - lastTime) / drawInterval;
 			timer += (currentTime - lastTime);
 			lastTime = currentTime;
-			
-			if(delta >=1) {
+
+			if (delta >= 1) {
 				update();
-				repaint();
+//				repaint();
+				drawToTempScreen();
+				drawToScreen();
 				delta--;
 				drawCount++;
 			}
-			
-			if(timer >= 1000000000) {
+
+			if (timer >= 1000000000) {
 //				System.out.println("FPS: "+ drawCount );
 				drawCount = 0;
-				timer =0;
+				timer = 0;
 			}
 		}
 	}
-	
+
 	public void update() {
 
-		//Player
+		// Player
 		player.update();
-		
-		//Npc
-		for(int i = 0;i<npc.length;i++) {
-			if(npc[i] != null) {
+
+		// Npc
+		for (int i = 0; i < npc.length; i++) {
+			if (npc[i] != null) {
 				npc[i].update();
 			}
 		}
 	}
-	
-	public void paintComponent(Graphics g) {
-		
-		super.paintComponent(g);
-		
-		Graphics2D g2 = (Graphics2D)g;
-	
-		// TILE
-		tileM.draw(g2);	// draw the world before player !!
-		
+
+	public void drawToTempScreen() {
+		tileM.draw(g2); // draw the world before player !!
+
 		// OBJECT
-		for(int i = 0; i < obj.length; i++) {
-			if(obj[i] != null) { // to avoid null pointer errors
+		for (int i = 0; i < obj.length; i++) {
+			if (obj[i] != null) { // to avoid null pointer errors
 				obj[i].draw(g2, this);
 			}
 		}
-		
-		//Npc
-		for(int i = 0; i<npc.length;i++) {
-			if(npc[i] !=null) {
+
+		// Npc
+		for (int i = 0; i < npc.length; i++) {
+			if (npc[i] != null) {
 				npc[i].draw(g2);
 			}
 		}
-		
+
 		// PLAYER
 		player.draw(g2);
-	
-		g2.dispose();
 	}
-	
+
+//	public void paintComponent(Graphics g) {
+//
+//		super.paintComponent(g);
+//
+//		Graphics2D g2 = (Graphics2D) g;
+//
+//		// TILE
+//		tileM.draw(g2); // draw the world before player !!
+//
+//		// OBJECT
+//		for (int i = 0; i < obj.length; i++) {
+//			if (obj[i] != null) { // to avoid null pointer errors
+//				obj[i].draw(g2, this);
+//			}
+//		}
+//
+//		// Npc
+//		for (int i = 0; i < npc.length; i++) {
+//			if (npc[i] != null) {
+//				npc[i].draw(g2);
+//			}
+//		}
+//
+//		// PLAYER
+//		player.draw(g2);
+//
+//		g2.dispose();
+//	}
+
+	public void drawToScreen() {
+		
+		Graphics g = getGraphics();
+		g.drawImage(tempScreen, 0, 0, screenWidth2, screenHeight2,null);
+		g.dispose();
+	}
 	// for background music
 	public void playMusic(int i) {
-		
+
 		sound.setFile(i); // get sound from file
 		sound.play(); // play sound
 		sound.loop(); // loop sound through game
 	}
-	
+
 	public void stopMusic() {
-		
+
 		sound.stop();
 	}
-	
+
 	// for item sound effect
 	public void playSE(int i) {
-		
+
 		sound.setFile(i);
 		sound.play();
 	}
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,14 +5,16 @@ import javax.swing.*;
 
 public class Main {
 
+	public static JFrame window;
 	//VV Adventure main program
     public static void main(String[] args) {
     	
     	
-        JFrame window = new JFrame();
+        window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("VV_Adventure");
+        window.setUndecorated(true);
         
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
