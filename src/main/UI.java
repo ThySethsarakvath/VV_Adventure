5a9525e6936bf67a1ef049fb505379cd790c24a6
DoDavin <dodavin96@gmail,com>
2025-07-07 16:41:08 +0700

    Create UI, handle Show Message

---
+++ src/main/UI.java
@@ -0,0 +1,70 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import object.OBJ_Door;
+
+public class UI {
+
+	GamePanel gp;
+	Graphics2D g2;
+	Font arial_40;
+//	BufferedImage doorImage;
+	public boolean messageOn = false;
+	public String message = "";
+	int messageCounter = 0;
+	
+	public UI(GamePanel gp) {
+		this.gp = gp;
+		
+		arial_40 = new Font("Arial", Font.PLAIN, 40);
+//		OBJ_Door door = new OBJ_Door(gp);
+//		doorImage = door.image;
+	}
+	
+	public void showMessage(String text) {
+		
+		message = text;
+		messageOn = true;
+		
+	}
+	
+	public void draw(Graphics2D g2) {
+		
+		this.g2 = g2;
+		
+		g2.setFont(arial_40);
+		g2.setColor(Color.white);
+		
+		if(gp.gameState == gp.playState) {
+			// Do playState stuff later
+		}
+		if(gp.gameState == gp.pauseState) {
+			drawPauseScreen();
+		}
+	}
+	
+	public void drawPauseScreen() {
+		
+		g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 80F));
+		String text = "PAUSED";
+		
+		int x = getXforCenteredText(text);
+		int y = gp.screenHeight / 2;
+		
+		g2.drawString(text, x, y);
+	
+	}
+	
+	public int getXforCenteredText(String text) {
+		
+		int length = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+		int x = gp.screenWidth / 2 - length / 2;
+		
+		return x;
+	}
+	
+}
