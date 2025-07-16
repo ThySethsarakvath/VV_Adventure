b95b3a5e76cd247254e46602652d3fa0b0c3a2f4
DoDavin <dodavin96@gmail,com>
2025-07-16 12:53:53 +0700

    Modified esc key to open option and added M key to open Menu

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.awt.desktop.OpenFilesEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -23,82 +24,121 @@ public class KeyHandler implements KeyListener {
 		
 		// TITLE STATE
 		if(gp.gameState == gp.titleState) {
-			
-			if(code == KeyEvent.VK_W) {
-				gp.ui.commandNum--;
-				if(gp.ui.commandNum < 0) {
-					gp.ui.commandNum = 2;
-				}
-			}
-			if(code == KeyEvent.VK_S) {
-				gp.ui.commandNum++;
-				if(gp.ui.commandNum > 2) {
-					gp.ui.commandNum = 0;
-				}
-			}
-			if(code == KeyEvent.VK_ENTER) {
-				if(gp.ui.commandNum == 0) {
-					gp.gameState = gp.playState;
-					gp.playMusic(0);
-				}
-				if(gp.ui.commandNum == 1) {
-					// add later
-				}
-				if(gp.ui.commandNum == 2) {
-					System.exit(0);
-				}
-			}
+			titleState(code);
 		}
 		
 		// PLAY STATE
 		if(gp.gameState == gp.playState) {
-			if(code == KeyEvent.VK_W) {
-				upPressed = true;
-			}
-			if(code == KeyEvent.VK_A) {
-				leftPressed = true	;	
-			}
-			if(code == KeyEvent.VK_S) {
-				downPressed = true;
-			}
-			if(code == KeyEvent.VK_D) {
-				rightPressed = true;
-			}
-			if (code == KeyEvent.VK_P) {
-				gp.gameState = gp.pauseState;
-				gp.music.pause();
-			}
-			if(code == KeyEvent.VK_ENTER) {
-				enterPressed = true;
-			}
-			if(code == KeyEvent.VK_F) {
-				fPressed = true;
-			}
-			if(code == KeyEvent.VK_ESCAPE) {
-				gp.gameState = gp.titleState;
-			}
+			playState(code);
 		}
 		
 		// PAUSE STATE
 		else if(gp.gameState == gp.pauseState) {
-			if (code == KeyEvent.VK_P) {
-				gp.gameState = gp.playState;
-				gp.music.resume();
-			}
+			pauseState(code);
 		}
 		
 		// DIALOGUE STATE
 		else if(gp.gameState == gp.dialogueState) {
-			if(code == KeyEvent.VK_ENTER) {
-				if(gp.ui.textCompleted) {
-					gp.gameState = gp.playState;
-				} else {
-					// Fast-forward typing if user presses before it's done
-					gp.ui.displayedText = gp.ui.currentDialogue;
-					gp.ui.textCompleted = true;
-				}
+			dialogueState(code);
+		}
+		
+		// OPTIONS STATE
+		else if(gp.gameState == gp.optionsState) {
+			optionsState(code);
+		}
+	}
+	
+	public void titleState(int code) {
+		
+		if(code == KeyEvent.VK_W) {
+			gp.ui.commandNum--;
+			if(gp.ui.commandNum < 0) {
+				gp.ui.commandNum = 2;
 			}
 		}
+		if(code == KeyEvent.VK_S) {
+			gp.ui.commandNum++;
+			if(gp.ui.commandNum > 2) {
+				gp.ui.commandNum = 0;
+			}
+		}
+		if(code == KeyEvent.VK_ENTER) {
+			if(gp.ui.commandNum == 0) {
+				gp.gameState = gp.playState;
+				gp.playMusic(0);
+			}
+			if(gp.ui.commandNum == 1) {
+				// add later
+			}
+			if(gp.ui.commandNum == 2) {
+				System.exit(0);
+			}
+		}
+	}
+	
+	public void playState(int code) {
+		
+		if(code == KeyEvent.VK_W) {
+			upPressed = true;
+		}
+		if(code == KeyEvent.VK_A) {
+			leftPressed = true	;	
+		}
+		if(code == KeyEvent.VK_S) {
+			downPressed = true;
+		}
+		if(code == KeyEvent.VK_D) {
+			rightPressed = true;
+		}
+		if (code == KeyEvent.VK_P) {
+			gp.gameState = gp.pauseState;
+			gp.music.pause();
+		}
+		if(code == KeyEvent.VK_ENTER) {
+			enterPressed = true;
+		}
+		if(code == KeyEvent.VK_F) {
+			fPressed = true;
+		}
+		if(code == KeyEvent.VK_ESCAPE) {
+			gp.gameState = gp.optionsState;
+		}
+		if(code == KeyEvent.VK_M) {
+			gp.gameState = gp.titleState;
+		}
+	}
+	
+	public void pauseState(int code) {
+		
+		if (code == KeyEvent.VK_P) {
+			gp.gameState = gp.playState;
+			gp.music.resume();
+		}
+	}
+	
+	public void dialogueState(int code) {
+		
+		if(code == KeyEvent.VK_ENTER) {
+			if(gp.ui.textCompleted) {
+				gp.gameState = gp.playState;
+			} else {
+				// Fast-forward typing if user presses before it's done
+				gp.ui.displayedText = gp.ui.currentDialogue;
+				gp.ui.textCompleted = true;
+			}
+		}
+	}
+	
+	public void optionsState(int code) {
+		
+		if(code == KeyEvent.VK_ESCAPE){
+			gp.gameState = gp.playState;
+		}
+		
+		if(code == KeyEvent.VK_ENTER) {
+			enterPressed = true;
+		}
+		
 	}
 
 	@Override
