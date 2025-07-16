97b60006c7a6f94861ce389155867135dc32f51a
DoDavin <dodavin96@gmail,com>
2025-07-17 00:46:56 +0700

    Implemented key for control options State

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -136,6 +136,52 @@ public class KeyHandler implements KeyListener {
 			enterPressed = true;
 		}
 		
+		int maxCommandNum = 0;
+		switch(gp.ui.subState) {
+		case 0: maxCommandNum = 5; break;
+		case 3: maxCommandNum = 1; break;
+		}
+		
+		if(code == KeyEvent.VK_W) {
+			gp.ui.commandNum--;
+//			gp.playSE(9);
+			if(gp.ui.commandNum < 0) {
+				gp.ui.commandNum = maxCommandNum;
+			}
+		}
+		
+		if(code == KeyEvent.VK_S) {
+			gp.ui.commandNum++;
+//			gp.playSE(9);
+			if(gp.ui.commandNum > maxCommandNum) {
+				gp.ui.commandNum = 0;
+			}
+		}
+		
+		if(code == KeyEvent.VK_A) {
+			if(gp.ui.subState == 0) {
+				if(gp.ui.commandNum == 1 && gp.music.volumeScale > 0) {
+					gp.music.volumeScale--;
+					gp.music.checkVolume();		
+				}
+				if(gp.ui.commandNum == 2 && gp.se.volumeScale > 0) {
+					gp.se.volumeScale--;		
+				}
+			}
+		}
+		
+		if(code == KeyEvent.VK_D) {
+			if(gp.ui.subState == 0) {
+				if(gp.ui.commandNum == 1 && gp.music.volumeScale < 5) {
+					gp.music.volumeScale++;
+					gp.music.checkVolume();		
+				}
+				if(gp.ui.commandNum == 2 && gp.se.volumeScale < 5) {
+					gp.se.volumeScale++;	
+				}
+			}
+		}
+		
 	}
 
 	@Override
