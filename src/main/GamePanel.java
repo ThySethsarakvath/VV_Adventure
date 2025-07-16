538623db35594916b7e28721dbeedfd275a09586
DoDavin <dodavin96@gmail,com>
2025-07-16 12:52:57 +0700

    Added Option State attribute

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -71,6 +71,7 @@ public class GamePanel extends JPanel implements Runnable {
 	public final int pauseState = 2;
 	// For dialogues
 	public final int dialogueState = 3;
+	public final int optionsState = 5;
 
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
