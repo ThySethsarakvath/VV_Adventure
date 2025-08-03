f2ccead3d11c80cec738c2ac37723c95f24d5a41
DoDavin <dodavin96@gmail,com>
2025-08-03 11:33:22 +0700

    Created generateParticle for generate Particle

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -136,6 +136,43 @@ public class Entity {
 		
 	}
 	
+	public Color getParticleColor() {
+		Color color = null;
+		return color;
+	}
+	
+	public int getParticleSize() {
+		int size = 0; 
+		return size;
+	}
+	
+	public int getParticleSpeed() {
+		int speed = 0;
+		return speed;
+	}
+	
+	public int getParticleMaxLife() {
+		int maxLife = 0;
+		return maxLife;
+	}
+	
+	public void generateParticle(Entity generator, Entity target) {
+		
+		Color color = generator.getParticleColor();
+		int size = generator.getParticleSize();
+		int speed = generator.getParticleSpeed();
+		int maxLife = generator.getParticleMaxLife();
+		
+		Particle p1 = new Particle(gp, generator, color, size, speed, maxLife, -2, -1);
+		Particle p2 = new Particle(gp, generator, color, size, speed, maxLife, 2, -1);
+		Particle p3 = new Particle(gp, generator, color, size, speed, maxLife, -2, 1);
+		Particle p4 = new Particle(gp, generator, color, size, speed, maxLife, 2, 1);
+		gp.particleList.add(p1);
+		gp.particleList.add(p2);
+		gp.particleList.add(p3);
+		gp.particleList.add(p4);
+	}
+	
 	public void update() {
 		setAction();
 		collisionOn = false;
@@ -143,6 +180,7 @@ public class Entity {
 		gp.cChecker.checkObject(this, false);
 		gp.cChecker.checkEntity(this, gp.monster);
 		gp.cChecker.checkEntity(this, gp.npc);
+		gp.cChecker.checkEntity(this, gp.iTile);
 		boolean contactPlayer = gp.cChecker.checkPlayer(this);
 
 		if (this.type == type_zombie && contactPlayer == true) {
@@ -185,7 +223,7 @@ public class Entity {
 		}
 		}
 
-		if (invincible) {
+		if (invincible == true) {
 			invinCounter++;
 			if (invinCounter > 40) {
 				invincible = false;
