41919ff7b7cd2fce4bf2747a5ec747b63ee3a4f4
DoDavin <dodavin96@gmail,com>
2025-08-25 09:59:34 +0700

    Fixed bug

--- src/main/Config.java
+++ src/main/Config.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class Config {
+public class Config implements Configurable{
 
 	GamePanel gp;
 	
---
+++ src/main/Configurable.java
@@ -0,0 +1,7 @@
+package main;
+
+public interface Configurable {
+    void saveConfig();
+    void loadConfig();
+}
+
